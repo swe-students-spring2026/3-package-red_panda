517d7edd48711236c44d018464006799337ba887
Landreani04 <Lda8263@nyu.edu>
2026-03-30 20:14:15 -0400

    Package artifacts

--- src/sciencii/__init__.py
+++ src/sciencii/__init__.py
@@ -2,6 +2,7 @@ from .dna_helix import dna_helix
 from .pipette import pipette
 from .test_tube import test_tube
 from .periodic_table import get_element
+from .molecule_caption import molecule_caption
 
-__all__ = ["dna_helix", "pipette", "test_tube", "get_element"]
+__all__ = ["dna_helix", "pipette", "test_tube", "get_element", "molecule_caption"]
 
